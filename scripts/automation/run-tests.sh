be05e411d4774df0bc43509ed4cc7dc9ac7ecf48
Erik Pearson <eapearson@lbl.gov>
2021-12-13 11:37:06 -0800

    try managing testing output directory from host script

--- scripts/automation/run-tests.sh
+++ scripts/automation/run-tests.sh
@@ -1,1 +1,6 @@
+# Manage the test output directory
+rm -rf testing_output
+mkdir testing_output
+chmod a+rwx testing_output
+# Run the tests w/in a Docker container
 docker compose -f docker-compose-unit_test.yml run --rm test_runner
--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -1,12 +1,17 @@
 set -e
+
 echo "Running Unit Tests"
+
 echo "> Cleaning testing output..."
-echo "> SKIPPED"
-# rm -rf testing_output/* || true
+rm -rf testing_output/* || true
+
 echo "> Running tests..."
 deno test --unstable --allow-read  --coverage=testing_output/cov_profile "${TEST_FILE:-.}"
+
 echo "> Generating coverage profile..."
 deno coverage testing_output/cov_profile --lcov > testing_output/cov_profile.lcov
+
 echo "> Generating coverage report..."
 genhtml -o testing_output/cov_profile/html testing_output/cov_profile.lcov
+
 echo "> DONE!"
