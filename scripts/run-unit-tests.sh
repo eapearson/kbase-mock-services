a46df887a198be25afe7d01e492b0d05d57ba674
Erik Pearson <eapearson@lbl.gov>
2021-12-13 10:32:42 -0800

    tolerate missing testing_output directory

--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -1,5 +1,5 @@
 set -e
-rm -rf testing_output/*
+rm -rf testing_output/* || true
 deno test --unstable --allow-read  --coverage=testing_output/cov_profile "${TEST_FILE:-.}"
 deno coverage testing_output/cov_profile --lcov > testing_output/cov_profile.lcov
 genhtml -o testing_output/cov_profile/html testing_output/cov_profile.lcov
