f37891b101b36a75b0b1e7533b1dc2bab26711e7
Erik Pearson <eapearson@lbl.gov>
2021-12-13 10:53:10 -0800

    try skipping testing output cleanup

--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -1,7 +1,8 @@
 set -e
 echo "Running Unit Tests"
 echo "> Cleaning testing output..."
-rm -rf testing_output/* || true
+echo "> SKIPPED"
+# rm -rf testing_output/* || true
 echo "> Running tests..."
 deno test --unstable --allow-read  --coverage=testing_output/cov_profile "${TEST_FILE:-.}"
 echo "> Generating coverage profile..."
